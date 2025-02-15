5e23f6f54d6f250599d76a45f03b2073a8de28e8
CharlesMich <charleedongo@hotmail.com>
2025-02-15 09:56:27 -0600

    Initial Commit

---
+++ wordle_2/wordle_2App.swift
@@ -0,0 +1,17 @@
+//
+//  wordle_2App.swift
+//  wordle_2
+//
+//  Created by Charles Michael on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct wordle_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
